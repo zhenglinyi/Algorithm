7492d68dd0fb5d906d02b0ff9e90f71dbcf01c46
zhenglinyi <372145879@qq.com>
2020-09-27 21:17:48 +0800

    Initial commit

---
+++ src/nb/leetcode/Solution.java
@@ -0,0 +1,3 @@
+package nb.leetcode;
+
+
---
+++ src/nb/leetcode/_206.java
@@ -0,0 +1,42 @@
+package nb.leetcode;
+
+/**
+ * @author shkstart
+ * @date 2020-09-22 23:43
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+//迭代
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
+//递归
+class Solution2 {
+    public ListNode reverseList(ListNode head) {
+        if(head==null||head.next==null)
+            return head;
+        ListNode temp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+}
+public class _206 {
+    public static void main(String[] args) {
+        System.out.println("待遇性");
+    }
+}
+
